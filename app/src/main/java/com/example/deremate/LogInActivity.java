aea3901dc7009358874a68ecd381ad21616558b0
Francisco <frtocco@uade.edu.ar>
2025-03-29 17:26:59 -0300

    finalizada la pantalla de log in

--- app/src/main/java/com/example/deremate/LogInActivity.java
+++ app/src/main/java/com/example/deremate/LogInActivity.java
@@ -1,8 +1,12 @@
 package com.example.deremate;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,15 +14,55 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LogInActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
         setContentView(R.layout.activity_log_in);
+
+        Button bLogIn = findViewById(R.id.bLogIn);
+        EditText usernameText = findViewById(R.id.etUsername);
+        EditText passwordText = findViewById(R.id.etPassword);
+        TextView forgotPassword = findViewById(R.id.tvForgotPassword);
+        TextView register = findViewById(R.id.tvRegister);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        register.setOnClickListener(v -> {
+
+        });
+
+        forgotPassword.setOnClickListener(v -> {
+
+        });
+
+        bLogIn.setOnClickListener(v -> {
+            // Log in, obtiene las credenciales al tocar el boton
+
+            String username = usernameText.getText().toString();
+            String password = passwordText.getText().toString();
+
+            if((username.isEmpty() || password.isEmpty())){
+                new AlertDialog.Builder(this)
+                        .setTitle("Atención")
+                        .setMessage("Ingrese un usuario y contraseña.")
+                        .setPositiveButton("OK", (dialog, which) -> {
+                            dialog.dismiss();
+                        })
+                        .show();
+            }else{
+                // Llamada a la API
+            }
+
+        });
+
+
     }
 }
